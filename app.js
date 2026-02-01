42f0ccefddd27b0c9fc75d61ec32e7b583109300
matt4est <matt.forrest@dimensionstechnologies.com>
2026-02-01 20:27:27 +0000

    #29858-2

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ define(function(require) {
 		'branchvariable',
 		'callcenter',
 		'conference',
+		'csvUploader',
 		'device',
 		'directory',
 		'eavesdrop',
@@ -39,17 +40,18 @@ define(function(require) {
 		'featurecodes',
 		'groups',
 		'media',
+		'mediaSelect',
 		'menu',
 		'misc',
 		'qubicle',
 		'resource',
+		'strategy',
+		'strategyHolidays',
+		'strategyHours',
 		'temporalset',
 		'timeofday',
 		'user',
-		'vmbox',
-		'strategy',
-		'strategyHolidays',
-		'strategyHours'
+		'vmbox'
 	];
 
 	require(_.map(appSubmodules, function(name) {
@@ -1809,6 +1811,48 @@ define(function(require) {
 									data: mediaToUpload.file
 								},
 								success: function(data, status) {
+
+									var $mediaSelect = template.find('#music_on_hold_media_id');
+
+									if (media && media.id) {
+										var newOption = $('<option/>', {
+											value: media.id,
+											text: media.name
+										});
+
+										// remove any existing option with same id
+										$mediaSelect.find('option[value="' + media.id + '"]').remove();
+
+										// find the correct insertion point (after fixed options)
+										var inserted = false;
+
+										$mediaSelect.find('option').each(function() {
+											var $opt = $(this);
+											var val = $opt.val();
+
+											// skip fixed/system options
+											if (!val || val === 'shoutcast' || val === '{{silenceMedia}}') {
+												return;
+											}
+
+											if ($opt.text().localeCompare(media.name, undefined, { sensitivity: 'base' }) > 0) {
+												$opt.before(newOption);
+												inserted = true;
+												return false;
+											}
+										});
+
+										// if it belongs at the end
+										if (!inserted) {
+											$mediaSelect.append(newOption);
+										}
+
+										// select the new media
+										$mediaSelect.val(media.id);
+
+										// update chosen
+										$mediaSelect.trigger('chosen:updated').trigger('change');
+									}
 									closeUploadDiv(media);
 								},
 								error: function(data, status) {
